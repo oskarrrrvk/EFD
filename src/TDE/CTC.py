5c464d7e92c799a7fce2f4f5a1302ca4e725d351
oscar <oherrerogordaliza21@gmail.com>
2025-11-09 14:09:52 +0100

    user shell completed

--- src/TDE/CTC.py
+++ src/TDE/CTC.py
@@ -1,2 +1,37 @@
 import socket
-import threading
+from threading import Thread
+import commands_shell as shell
+
+ip=""
+port=0
+
+def read_config_file():
+    with open('socket_server.conf','r') as config_file:
+        lines = config_file.readlines()
+        global ip
+        global port
+        ip,port = lines[0].split(':')
+
+def client_socket():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip,int(port)))
+    loop = True
+    while loop:
+        if shell.movement == "exit":
+            loop = False
+        else:
+            if shell.movement != "":
+                s.sendall(shell.movement.encode('utf-8'))
+                shell.movement = ""
+    s.close()
+
+
+if __name__ == "__main__":
+    read_config_file()
+    server_thread = Thread(name="Server",target=client_socket)
+    commands_thread = Thread(name="Commands",target=shell.read_movement)
+
+    server_thread.start()
+    commands_thread.start()
+    commands_thread.join()
+    server_thread.join()
---
+++ src/TDE/commands_shell.py
@@ -0,0 +1,52 @@
+import os
+import time
+
+MOVEMENTS = [
+    "I E1 S1/2",
+    "I E1 S2/2",
+    "I E1 B",
+    "I S1/2 B",
+    "I S2/2 B",
+    "I E2 S1/1",
+    "I E2 S2/1",
+    "I E2 B",
+    "I S1/1 B",
+    "I S2/1 B",
+    "M E1 S1/2",
+    "M E1 S2/2",
+    "M S1/2 B", 
+    "M S2/2 B",
+    "M E2 S1/1",
+    "M E2 S2/1",
+    "M S1/1 B",
+    "M S2/1 B",
+    "R E1 S1/2",
+    "R E1 S2/2",
+    "R E2 S1/1",
+    "R E2 S2/1",
+    "exit"
+]
+
+movement = ""
+
+def test_movement(command):
+    return  command in MOVEMENTS
+
+def read_movement():
+    global movement
+    loop = True
+    while loop:
+        print("COMMAND INTERFACE")
+        command = input(">")
+        if test_movement(command):
+            movement = command
+            print("movement success")
+        else:
+            print("movement incorrect")
+
+        if command == "exit":
+            loop = False
+        
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
