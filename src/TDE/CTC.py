55db9a52c5b4340dc4f04b37e2a94433cb7d8e09
oscar <oherrerogordaliza21@gmail.com>
2025-11-11 19:19:58 +0100

    CTC.py corrected

--- src/TDE/CTC.py
+++ src/TDE/CTC.py
@@ -4,6 +4,7 @@ import commands_shell as shell
 
 ip=""
 port=0
+s=None
 
 def read_config_file():
     with open('socket_server.conf','r') as config_file:
@@ -12,9 +13,14 @@ def read_config_file():
         global port
         ip,port = lines[0].split(':')
 
-def client_socket():
+def config_client_socket():
+    global s
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((ip,int(port)))
+    print("Connecting to server")
+    while s.connect_ex((ip,int(port))) != 0:
+        pass
+    print("\nConnected to server")
+def client_socket():
     loop = True
     while loop:
         if shell.movement == "exit":
@@ -28,6 +34,7 @@ def client_socket():
 
 if __name__ == "__main__":
     read_config_file()
+    config_client_socket()
     server_thread = Thread(name="Server",target=client_socket)
     commands_thread = Thread(name="Commands",target=shell.read_movement)
 
